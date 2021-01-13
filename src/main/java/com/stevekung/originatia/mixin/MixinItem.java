6d5a8074d18b8844f84894ec6090819948498fb6
SteveKunG <mccommander_minecraft@hotmail.com>
2021-01-13 18:07:20 +0700

    Improved Paper Item placement

---
+++ src/main/java/com/stevekung/originatia/mixin/MixinItem.java
@@ -0,0 +1,22 @@
+package com.stevekung.originatia.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.stevekung.originatia.item.PaperItemHandler;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResultType;
+
+@Mixin(Item.class)
+public class MixinItem
+{
+    @Inject(method = "onItemUse(Lnet/minecraft/item/ItemUseContext;)Lnet/minecraft/util/ActionResultType;", cancellable = true, at = @At("HEAD"))
+    private void onItemUse(ItemUseContext context, CallbackInfoReturnable<ActionResultType> info)
+    {
+        PaperItemHandler.INSTANCE.onItemUse(context, info);
+    }
+}
