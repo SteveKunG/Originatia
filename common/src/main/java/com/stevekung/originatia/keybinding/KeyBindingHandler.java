6b7f18be0a4351b03a5df2c888e8f04bc238de3d
SteveKunG <mccommander_minecraft@hotmail.com>
2021-05-10 19:59:19 +0700

    Update key binding hooks

--- common/src/main/java/com/stevekung/originatia/keybinding/KeyBindingHandler.java
+++ common/src/main/java/com/stevekung/originatia/keybinding/KeyBindingHandler.java
@@ -2,7 +2,8 @@ package com.stevekung.originatia.keybinding;
 
 import org.lwjgl.glfw.GLFW;
 import com.stevekung.originatia.core.OriginatiaMod;
-import me.shedaniel.architectury.registry.KeyBindings;
+import com.stevekung.stevekungslib.client.KeyMappingBase;
+import com.stevekung.stevekungslib.utils.client.ClientRegistryUtils;
 import net.minecraft.client.KeyMapping;
 
 public class KeyBindingHandler
@@ -11,8 +12,7 @@ public class KeyBindingHandler
 
     public static void init()
     {
-        KeyBindingHandler.KEY_QUICK_NAVIGATOR = new KeyMapping("key.quick_navigator.desc", GLFW.GLFW_KEY_N, OriginatiaMod.MOD_ID);
-
-        KeyBindings.registerKeyBinding(KeyBindingHandler.KEY_QUICK_NAVIGATOR);
+        KeyBindingHandler.KEY_QUICK_NAVIGATOR = new KeyMappingBase("key.quick_navigator.desc", GLFW.GLFW_KEY_N, OriginatiaMod.MOD_ID);
+        ClientRegistryUtils.registerKeyBinding(KeyBindingHandler.KEY_QUICK_NAVIGATOR);
     }
 }
